2c4a299b9f65675ca06ec3fe5ab09de02e6289d3
David Hayes <heymrhayes@gmail.com>
2018-05-30 15:04:22 +0000

    script work

---
+++ pages.js
@@ -0,0 +1,10 @@
+// page data
+
+const pages = [
+    { "name": "Home" },
+    { "name": "Student Profile" },
+    { "name": "Contacts" },
+    { "name": "Documents" },
+    { "name": "Directory Opt-In/Out" },
+    { "name": "Recruiter Opt-In/Out" }
+];
