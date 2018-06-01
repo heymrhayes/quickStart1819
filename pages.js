8b66135003f7adeb441e5eff2f7f18300b66b14a
David Hayes <heymrhayes@gmail.com>
2018-06-01 22:35:29 +0000

    home page

--- pages.js
+++ pages.js
@@ -1,10 +1,15 @@
 // page data
 
 const pages = [
-    { "name": "Home" },
+    { "name": "Home"},
     { "name": "Student Profile" },
     { "name": "Contacts" },
     { "name": "Documents" },
     { "name": "Directory Opt-In/Out" },
     { "name": "Recruiter Opt-In/Out" }
 ];
+
+const pageDetails = {
+    "home" : {"displayName": "Home", "desc" : "Shows your progress through the process"},
+    "profile" : {"displayName": "Student Profile", "desc": "View and update basic student information"}
+}
